f4b493e60bb1596a3691f569e559d21496af6c31
Ramindu Nimesh <117975788+ramindu-git@users.noreply.github.com>
2023-03-18 18:49:52 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -6,6 +6,17 @@
 
 int main() {
   
-  return 0;
+	float mark1, mark2, average = 0;
+	
+	printf("Enter first subject mark : ");
+	scanf("%f", &mark1);
+	
+	printf("Enter second subject mark : ");
+	scanf("%f", &mark2);
+	
+	average = (mark1 + mark2)/2;
+	
+	printf("Average of the marks : %.2f", average);
+   
+   return 0;
 }
-
