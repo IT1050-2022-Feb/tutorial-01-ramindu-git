7cbc585daf382af5f56353b693ba3deb7dd1304a
Ramindu Nimesh <117975788+ramindu-git@users.noreply.github.com>
2023-03-18 18:58:51 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -20,5 +20,22 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
   
+  int distance;
+	float amount = 0;
+	
+	printf("Enter Distance : ");
+	scanf("%d", &distance);
+	
+	if(distance <= 30) {
+		
+		amount = 50 * distance;
+	}
+	else {
+		
+		amount = (30 * 50)+ ((distance - 30) * 40);
+	}
+	
+	printf("Amount : %.2f", amount);
+  
   return 0;
 }
